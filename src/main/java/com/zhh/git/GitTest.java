1c1c4a9bc2f24b297a87eb626c283825cda5aa1d
zhanghua-069 <13815005969@163.com>
2021-12-05 16:22:31 +0800

    github-push commit

--- src/main/java/com/zhh/git/GitTest.java
+++ src/main/java/com/zhh/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("second commit");
         System.out.println("third commit");
         System.out.println("hotfix commit");
+        System.out.println("github push");
     }
 }
