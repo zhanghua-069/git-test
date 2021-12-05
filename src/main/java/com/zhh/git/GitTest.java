eed23985940d839f07d8c07d411dfae74826b8c3
zhanghua-069 <13815005969@163.com>
2021-12-05 15:22:39 +0800

    third commit

--- src/main/java/com/zhh/git/GitTest.java
+++ src/main/java/com/zhh/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
